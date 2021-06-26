2560478fae8857ebde1b07242f272e0844f4e982
Divij Jain <68986989+diiviij@users.noreply.github.com>
2021-06-26 15:32:59 +0530

    How to Click on  button

---
+++ click-elements.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+from selenium import webdriver
+
+
+browser = webdriver.Firefox()
+
+browser.implicitly_wait(5)
+
+
+browser.get('https://www.instagram.com/')
+
+
+login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+
+login_link.click()
+
+
+sleep(5)
+
+
+browser.close()
